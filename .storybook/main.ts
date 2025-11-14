c3ea36a555c8eb016ef5b9200eec09b6777b5b14
Ramz001 <rkenjaev1@gmail.com>
2025-11-14 07:01:02 +0500

    Update dependencies and configuration for Storybook; remove package-lock.json and add storybook-static to .gitignore.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,15 +3,12 @@ import type { StorybookConfig } from '@storybook/react-vite';
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    '@storybook/addon-onboarding',
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
     '@chromatic-com/storybook',
-    '@storybook/addon-interactions',
+    '@storybook/addon-docs',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-a11y',
+    '@storybook/addon-vitest',
   ],
-  framework: {
-    name: '@storybook/react-vite',
-    options: {},
-  },
+  framework: { name: '@storybook/react-vite', options: {} },
 };
 export default config;
